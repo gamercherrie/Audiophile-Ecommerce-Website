d7deb7c9b8378487c86882f3327d8b6c2106de16
Charisse Calma <charissecalma05@gmail.com>
2022-12-27 00:41:07 -0800

    Trying to make the Product Modals dynamic by using the JSON file for informarion

--- src/components/shared/ProductAddToCartModal/ProductAddToCartModal.jsx
+++ src/components/shared/ProductAddToCartModal/ProductAddToCartModal.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './ProductAddToCartModal.scss'
+import data from '../../../local-json/data.json'
 
 
 const ProductAddToCartModal = (props) => {
@@ -8,45 +9,19 @@ const ProductAddToCartModal = (props) => {
   const navigate = useNavigate()
 
   const { ImageofProduct, newProduct, productTitle, productDescription} = props
-  const [price, setPrice] = useState(null)
+  const [price, setPrice] = useState('')
 
-  useEffect (() => {
-    async function fetchData() {
-      const response = await fetch('src/local-json/data.json');
-      const data = await response.json();
-
-      // Map the data from the JSON file to an array of objects
-      const objects = data.map(item => ({
-        id: item.id,
-        slug: item.slug,
-        name: item.name,
-        image: item.image,
-        category: item.category,
-        categoryImage: item.categoryImage,
-        new: item.new,
-        price: item.price,
-        description: item.description,
-        features: item.features,
-        includes: item.includes,
-        gallery: item.gallery,
-        others: item.others
-      }));
-
-      // Find the object with the name specified in the objectName prop
-      const object = objects.find(item => item.name === productTitle);
-      setPrice(object.price);
-    }
-    fetchData();
-  }, [productTitle]);
+  const selectedProduct = data.filter(data => data.name.toLowerCase() === productTitle.toLowerCase())[0]
+  const productPrice = selectedProduct.price
 
   return (
     <div className="add__container">
        <button onClick={() => navigate(-1)}>Go Back</button>
-       <img src={ImageofProduct} alt="A preview of the Product"/>
+       <img src={selectedProduct.image.mobile}  alt={selectedProduct.name}/>
        <p>{newProduct}</p>
        <h1>{productTitle}</h1>
        <p>{productDescription}</p>
-       <span>${price ? price : null}</span>
+       <span>${productPrice}</span>
     </div>
   )
 }
