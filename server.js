68db8db0ddcd3267f1ad964c68da9486f95179a7
Charisse Calma <charissecalma05@gmail.com>
2023-02-01 00:39:32 -0800

    Trying something

--- server.js
+++ server.js
@@ -4,19 +4,12 @@ const bcrypt = require('bcrypt');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
-const path = require('path');
 
 require('dotenv').config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(cors());
-
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
 
 const port = process.env.PORT || 3001;
 const uri = process.env.URI;
