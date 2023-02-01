ef85e8b185a9dbb6abe557874de60a5598a217fa
Charisse Calma <charissecalma05@gmail.com>
2023-02-01 03:31:20 -0800

    whatever works

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 const port = process.env.PORT || 3001;
