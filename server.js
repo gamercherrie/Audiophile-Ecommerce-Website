ef152b5c758c4eb5f4beaf95c05ed720e34f0934
Charisse Calma <charissecalma05@gmail.com>
2023-02-01 02:38:37 -0800

    Lets try a different host

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const bcrypt = require('bcrypt');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
 
 require('dotenv').config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, './client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 const port = process.env.PORT || 3001;
 const uri = process.env.URI;
