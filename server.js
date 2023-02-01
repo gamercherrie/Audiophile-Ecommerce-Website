35aacdf327cfa8810ef1a96bf1a4e6c66795f416
Charisse Calma <charissecalma05@gmail.com>
2023-01-31 18:21:12 -0800

    pls deploy

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(cors());
 
 const port = process.env.PORT || 3001
-const uri = process.env.uri;
+const uri = process.env.URI;
 
 async function connect(){
     try{
